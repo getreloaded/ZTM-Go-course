0882b88c0d41cbcd83ac3d71fc5f8852734db958
getreloaded <108175916+getreloaded@users.noreply.github.com>
2023-05-01 11:55:47 +0530

    First attempt at Library problem
    All the functionality mentioned in the problem statement is solved. Looks like a newbie program though compared to the solution provided by JL. Will rewrite the code including extra functionality shown in the solution compared to the problem statement.

--- lectures/exercise/sr-library/library.go
+++ lectures/exercise/sr-library/library.go
@@ -19,8 +19,71 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Book struct {
+	name        string
+	status      string
+	status_time time.Time
+}
+
+type Member struct {
+	name string
+}
+
+type Library struct {
+	books   []Book
+	members []Member
+}
+
+func checkin(book *Book) Book {
+	book.status = "In Library"
+	book.status_time = time.Now()
+	return *book
+}
+
+func checkout(book *Book, member *Member) Book {
+	book.status = "With" + member.name
+	book.status_time = time.Now()
+	return *book
+}
+
+func libraryPrinter(library *Library) {
+	fmt.Println("Books:")
+	for _, book := range library.books {
+		fmt.Println(book.name, book.status, book.status_time.String())
+	}
+	fmt.Println("Members:")
+	for i, member := range library.members {
+		fmt.Println(i+1, member.name)
+	}
+}
 
 func main() {
 
+	booklist := make([]Book, 4)
+	booklist[0] = Book{name: "Alice in Wonderland", status: "In Library", status_time: time.Now()}
+	booklist[1] = Book{name: "Bob in Wonderland", status: "In Library", status_time: time.Now()}
+	booklist[2] = Book{name: "Charlie in Wonderland", status: "In Library", status_time: time.Now()}
+	booklist[3] = Book{name: "David in Wonderland", status: "In Library", status_time: time.Now()}
+
+	memberlist := make([]Member, 3)
+	memberlist[0].name = "Scooby"
+	memberlist[1].name = "Daffy"
+	memberlist[2].name = "Bugs Bunny"
+
+	library := Library{books: booklist, members: memberlist}
+
+	libraryPrinter(&library)
+
+	checkout(&booklist[0], &memberlist[0])
+
+	libraryPrinter(&library)
+
+	checkin(&booklist[0])
+
+	libraryPrinter(&library)
 }
