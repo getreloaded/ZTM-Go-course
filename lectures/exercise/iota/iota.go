c961f7552621f3670240c4b4f1376aea94a2a2ef
getreloaded <108175916+getreloaded@users.noreply.github.com>
2023-05-04 09:31:22 +0530

    Update iota.go

--- lectures/exercise/iota/iota.go
+++ lectures/exercise/iota/iota.go
@@ -16,12 +16,45 @@ package main
 
 import "fmt"
 
+const (
+	add = iota
+	sub
+	mul
+	div
+)
+
+type Operation uint
+
+//   - Write a receiver function that performs the mathematical operation
+//     on two operands
+func (op Operation) calculate(x, y int) int {
+
+	switch op {
+	case add:
+		return x + y
+	case sub:
+		return x - y
+	case mul:
+		return x * y
+	case div:
+		return x / y
+	default:
+		panic("not implemented/ invalid operation")
+
+	}
+}
+
 func main() {
+
+	add := Operation(add)
 	fmt.Println(add.calculate(2, 2)) // = 4
 
-	fmt.Println(sub.calculate(10, 3)) // = 7
+	sub := Operation(sub)
+	fmt.Println(sub.calculate(10, 3)) // = 7)
 
+	mul := Operation(mul)
 	fmt.Println(mul.calculate(3, 3)) // = 9
 
+	div := Operation(div)
 	fmt.Println(div.calculate(100, 2)) // = 50
 }
