bf1d11406a33d11efc4fd76b35ae6784d538cb02
getreloaded <kshitijbafna1991@gmail.com>
2023-05-17 15:08:10 +0530

    exercise sync finished using channels

--- lectures/exercise/sync/sync.go
+++ lectures/exercise/sync/sync.go
@@ -19,7 +19,64 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+	"sync"
+	"unicode"
 )
 
-func main() {}
+// type Count struct {
+// 	count int
+// 	sync.Mutex
+// }
+
+func lettercounter(word string) int {
+	wordcopy := word
+	total := 0
+	for _, c := range wordcopy {
+		if unicode.IsLetter(c) {
+			total += 1
+		}
+	}
+	return total
+}
+
+func calculate(w <-chan string, r chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	wordcopy := <-w
+	r <- lettercounter(wordcopy)
+}
+
+func main() {
+	textInput := bufio.NewScanner(os.Stdin)
+	var wg sync.WaitGroup
+	wordCh := make(chan string, 10)
+	resultCh := make(chan int, 10)
+	totalLetters := 0
+
+	for textInput.Scan() {
+		line := textInput.Text()
+		wordlist := strings.Split(line, " ")
+
+		for _, word := range wordlist {
+			wordCh <- word
+			wg.Add(1)
+			go calculate(wordCh, resultCh, &wg)
+		}
+	}
+	wg.Wait()
+	//close(wordCh)
+	//close(resultCh)
+
+	for {
+		r, ok := <-resultCh
+		if ok {
+			totalLetters += r
+		} else {
+			break
+		}
+	}
+	fmt.Println(totalLetters, "letters")
+}
